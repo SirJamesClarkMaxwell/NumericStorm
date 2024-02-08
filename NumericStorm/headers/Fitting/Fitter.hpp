2fc4ee5e985283f07027c05ee8b7825724760b12
Franchessco <fzabier@gmail.com>
2024-02-08 23:17:34 +0100

    added rest of Fitter class

--- NumericStorm/headers/Fitting/Fitter.hpp
+++ NumericStorm/headers/Fitting/Fitter.hpp
@@ -9,11 +9,14 @@ class Fitter
 {
 public:
 	Fitter() = default;
-
+	~Fitter() {};
+	virtual Parameters& <size_p> getFittedParameters() = 0;
+	virtual minimize() = 0;
+	virtual setUp(FitterSettings&<size_p> fitterSettings) = 0;
 protected:
-	Parameters&<size_p> fittedParamters;
-	Parameters& <size_p> proposedParameters;
-	
+	Parameters& <size_p> m_fittedParamters;
+	Parameters& <size_p> m_proposedParameters;
+	FitterSettings& <size_p> m_fitterSettings;
 
 };
 
