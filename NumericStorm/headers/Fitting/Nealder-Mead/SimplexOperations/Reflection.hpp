349c9a00956d135866ac5d65c684925e47e5ba14
Franchessco <fzabier@gmail.com>
2024-02-10 22:37:24 +0100

    fixing small bugs, and some optimasition

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/Reflection.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/Reflection.hpp
@@ -21,19 +21,19 @@ template<size_t figure_size>
 SimplexFigure<figure_size> Reflection<figure_size>::operation (const SimplexOperationArguments& arguments)
 {
 	SimplexFigure<figure_size> reflectedFigure(m_simplexFigure);
-	reflectedFigure.sort();
+	
 	double alpha = arguments.getFactor();
 	SimplexPoint<figure_size - 1> centroid = reflectedFigure.getCentroid();
-
+	SimplexPoint<figure_size - 1> pointToReflectArround(centroid);
 	#if DEBUG
 		auto difference = centroid - reflectedFigure[0];
 		auto multiplicated = difference * alpha;
-		auto reflectedPoint = centroid + multiplicated;
+		auto pointToReflectArround += multiplicated;
 	#else if REALASE
-		SimplexPoint<figure_size - 1> reflectedPoint = centroid + (centroid - reflectedFigure[0]) * alpha;
+	pointToReflectArround += (centroid - reflectedFigure[0]) * alpha;
 	#endif
 
-		reflectedFigure[0] = reflectedPoint;
+		reflectedFigure[0] = pointToReflectArround;
 		return reflectedFigure;
 			
 		
