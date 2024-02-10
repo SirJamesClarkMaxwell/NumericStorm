08f2b7d1bdf356108e8a2d0e1ab8386dada40ff7
Franchessco <fzabier@gmail.com>
2024-02-10 12:56:42 +0100

    added extencible factory patern impementation and reflection implementation. We need to orginize a little bit our files into folders, and definitely add tests and update class diagram

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexFigure.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexFigure.hpp
@@ -1,24 +1,44 @@
 #pragma once
-#include "Fitting.hpp"
-
+#include "SimplexPoint.hpp"
+#include <numeric>
 
 namespace NumericStorm {
 namespace Fitting {
+
+
 template<size_t figure_size>
 class SimplexFigure {
 public:
-	SimplexFigure() = default;
-	SimplexFigure(std::array<SimplexPoint<figure_size-1>,figure_size> points)
-	:m_points(points){};
-	void sort(bool reverse = true) 
-	{
-		std::sort(m_points.begin(), m_points.end());
-		if (reverse)
-			std::reverse(m_points.begin(), m_points.end());
-	}
+	SimplexFigure(std::array<SimplexPoint<figure_size - 1>,figure_size> points)
+	:m_points(points)
+	{m_centroid = calculateCentroid();};
+
+	void sort(bool reverse = true);
+	SimplexPoint<figure_size - 1> getCentroid() { return m_centroid; }
 protected:
 	std::array<SimplexPoint<figure_size - 1>, figure_size> m_points;
-	SimplexPoint<figure_size - 1> m_proposalPoint;
+	SimplexPoint<figure_size - 1> m_centroid;
+private:
+	SimplexPoint<figure_size - 1> calculateCentroid();
+
 };
+template<size_t figure_size>
+void SimplexFigure<figure_size>::sort(bool reverse) 
+{
+	std::sort(m_points.begin(), m_points.end());
+	if (reverse)
+		std::reverse(m_points.begin(), m_points.end());
+};
+
+template<size_t figure_size>
+SimplexPoint<figure_size - 1> SimplexFigure<figure_size>::calculateCentroid() 
+{
+	#if DEBUG
+		auto& sum = std::accumulate(m_points);
+		auto& toReturn = sum / (figure_size - 1);
+		return toReturn;
+	#endif
+	return std::accumulate(m_points.begin(),m_points.end(),0) / (figure_size - 1)
+}
 }
 }
