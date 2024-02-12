0b263b7e8b0bea1cfd1fe7af35f9f4a301787bc3
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-12 21:59:50 +0100

    just added later on we need to change it to somethig like this using CreateObjectCallback = std::function<IGameObject*()>; in extensible factory patterns

--- NumericStorm/headers/Fitting/Nealder-Mead/BasicSimplex/BasicSimplexFitter.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/BasicSimplex/BasicSimplexFitter.hpp
@@ -1,8 +1,23 @@
 #pragma once
+
+#include "../SimplexFigure.hpp"
+#include "../SimplexFitter.hpp"
+#include "../SimplexPoint.hpp"
+
+
 namespace NumericStorm 
 {
 namespace Fitting 
 {
+template<size_t figure_size>
+class BasicSimplexFitter:public SimplexFitter<figure_size>
+{
+public:
+	BasicSimplexFitter(SimplexFigure<figure_size> simplexFigure) 
+		:SimplexFitter<figure_size>(simplexFigure){}
+
+
+};
 
 }
 }
