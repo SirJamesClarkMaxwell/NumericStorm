dfdab6c59faa913a591e7d79f9947b8d0bc34419
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-14 19:51:58 +0100

    added big changes of the architecture of dependencies between parameters and SimplexPoint. added todo

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexPoint.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexPoint.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include "../Parameters.hpp"
+#include "../Model.hpp"
+#include "../ErrorModel.hpp"
+
 #include <concepts>
 #include <type_traits>
+#include <memory>
+
 
 namespace NumericStorm 
 {
@@ -9,14 +14,15 @@ namespace Fitting
 {
 
 
-	template <std::size_t parameter_size>
-	class SimplexPoint : public Parameters<parameter_size> {
-	public:
-		using Parameters<parameter_size>::m_parameters; 
+template <std::size_t parameter_size>
+class SimplexPoint : {
+public:
 
 		SimplexPoint() = default;
 		SimplexPoint(std::array<double, parameter_size> parameters)
-			: Parameters<parameter_size>(parameters) {};
+			: m_parameters(parameters),m_error(-1) {};
+		std::array<double, parameter_size> getParameters() 
+			{ return m_parameters.getParameters();}
 
 		// Operator overloads
 		double& operator[](int index) {
@@ -27,25 +33,28 @@ namespace Fitting
 			return (index >= 0 && static_cast<std::size_t>(index) < parameter_size) ? m_parameters[index] : m_parameters[0];
 		}
 
-		bool operator==(const Parameters<parameter_size>& other) const { return 
-			m_parameters == other.getParameters(); }
-		bool operator==(const std::array<double, parameter_size>& other) const {
-			return m_parameters == other;
-		};
-		bool operator <=> (const Parameters<parameter_size>& other) const 
+		bool operator ==(const SimplexPoint<parameter_size>& other) const 
+			{ return m_parameters.getParameters() == other.getParameters(); }
+		bool operator == (const std::array<double, parameter_size>& other) const 
+			{return m_parameters.getParameters() == other;};
+		bool operator <=> (const SimplexPoint<parameter_size>& other) const 
 			{return m_error <=> other.m_error;}
-		SimplexPoint<parameter_size>& operator=(const Parameters<parameter_size>& other) {
+		SimplexPoint<parameter_size>& operator=(const SimplexPoint<parameter_size>& other) {
 			if (this != &other)
+			{
 				m_parameters = other.getParameters();
+				m_error = other.m_error;
+			}
+				
 			return *this;
 		}
 
-		SimplexPoint<parameter_size>& operator+=(const Parameters<parameter_size>& other) {
+		SimplexPoint<parameter_size>& operator+=(const SimplexPoint<parameter_size>& other) {
 			for (int i = 0; i < parameter_size; ++i)
 				m_parameters[i] += other[i];
 			return *this;
 		}
-		SimplexPoint<parameter_size>& operator-=(const Parameters<parameter_size>& other) {
+		SimplexPoint<parameter_size>& operator-=(const SimplexPoint<parameter_size>& other) {
 			for (int i = 0; i < parameter_size; ++i)
 				m_parameters[i] -= other[i];
 			return *this;
@@ -61,26 +70,38 @@ namespace Fitting
 			return *this;
 		}
 
-		SimplexPoint<parameter_size>& operator+(const Parameters<parameter_size>& other) const {
+		SimplexPoint<parameter_size> operator+(const SimplexPoint<parameter_size>& other) const {
 			SimplexPoint result = *this;
 			result += other;
 			return result;
 		}
-		SimplexPoint<parameter_size>& operator-(const Parameters<parameter_size>& other) const {
+		SimplexPoint<parameter_size> operator-(const SimplexPoint<parameter_size>& other) const {
 			SimplexPoint result = *this;
 			result -= other;
 			return result;
 		}
-		SimplexPoint<parameter_size>& operator*(const auto& other) const {
+		SimplexPoint<parameter_size> operator*(const auto& other) const {
 			SimplexPoint result = *this;
 			result *= other;
 			return result;
 		}
-		SimplexPoint<parameter_size>& operator/(const auto& other) const {
+		SimplexPoint<parameter_size> operator/(const auto& other) const {
 			SimplexPoint result = *this;
 			result /= other;
 			return result;
 		}
+	private:
+		Parameters<parameters_size> m_parameters;
+		double m_error;
+		bool m_modelSet;
+		bool m_errorModelSet;
+		std::unique_ptr<Model> m_model;
+		std::unique_ptr<ErrorModel> m_errorModel;
+		//TODO add functions to setting model
+		//TODO add functions to setting error model
+		//TODO initialize bools as false
+		//TODO add setUp function
+		//TODO add function to calculating data and error model
 
 	};
 
--- NumericStorm/headers/Fitting/Parameters.hpp
+++ NumericStorm/headers/Fitting/Parameters.hpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <iostream>
 #include <stdexcept>
+
 #include "Data.hpp"
 #include "Model.hpp"
 #include "ErrorModel.hpp"
@@ -10,13 +11,10 @@
 namespace NumericStorm {
 namespace Fitting {
 
-    template <size_t parameter_size>
-    class Model;
-
 template <size_t parameter_size>
 class Parameters {
 public:
-    
+
     Parameters(std::array<double, parameter_size> parameters)
         : m_parameters(parameters), m_error(-1)
         /*, m_ModelSet(false), m_ErrorModelSet(false), m_errorModel(nullptr), m_model(nullptr)*/ {};
@@ -25,34 +23,18 @@ public:
     virtual std::array<double, parameter_size> getParameters() = 0;
 
     virtual double getError() = 0;
-//    virtual void setUp() = 0;
-
-//    virtual void calculateError(const Data* referenceData) = 0;
-
+    double& operator[](int index) 
+    {
+        return (index >= 0 && static_cast<std::size_t>(index) < parameter_size) ? m_parameters[index] : m_parameters[0];
+    }
+    const double& operator[](int index) const 
+    {
+        return (index >= 0 && static_cast<std::size_t>(index) < parameter_size) ? m_parameters[index] : m_parameters[0];
+    }
 protected:
     std::array<double, parameter_size> m_parameters;
     double m_error;
-    AdditionalParameters m_additionalParameters;
-    //! we are thinking that this code below should be in a separate class 
-    //! later we should add this 
-/*
-    Model<parameter_            ze>* m_model;
-    ErrorModel* m_errorModel;
-    bool m_ModelSet;
-    bool m_ErrorModelSet;
-
-protected:
-    void setModel(Model<parameter_size>* modelToSet) 
-    {
-        m_model = modelToSet;
-        m_ModelSet = true;
-    };
-    void setErrorModel(ErrorModel* errorModelToSet)
-    {
-        m_errorModel = errorModelToSet;
-        m_ErrorModelSet = true;
-    };
-*/
 };
+
 }
 }
