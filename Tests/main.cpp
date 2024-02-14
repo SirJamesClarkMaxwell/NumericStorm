3f72b3a42ad3e5f5fade12769be3710eae1abb06
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-14 21:56:40 +0100

    fixed namespace problem

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,8 +1,9 @@
 #include "pch.h"
 
 
-
+using namespace NumericStorm::Fitting;
 TEST(SomeTest, NameForSomeTest) 
 {
 	EXPECT_DOUBLE_EQ(3.3, NumericStorm::add(1.1, 2.2),0.001);
+	std::cout << "some text" << std::endl;
 }
