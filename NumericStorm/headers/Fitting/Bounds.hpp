224fcb5c41c5cdeec5e8635a30b819acd789de78
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-14 19:51:58 +0100

    added big changes of the architecture of dependencies between parameters and SimplexPoint. added todo

--- NumericStorm/headers/Fitting/Bounds.hpp
+++ NumericStorm/headers/Fitting/Bounds.hpp
@@ -5,12 +5,12 @@ namespace NumericStorm
 {
 namespace Fitting 
 {
-template<size_t size_b>
-class Bounds :public Parameters<size_b>
+template<size_t bounds_size>
+class Bounds :public Parameters<bounds_size>
 {
 public:
-	Bounds(std::array<double, size_b> bounds)
-		:Parameters<size_b>(bounds);
+	Bounds(std::array<double, bounds_size> bounds)
+		:Parameters<bounds_size>(bounds);
 
 	virtual double& operator [](int index) = 0;
 	virtual std::array<double, size_p> getParameters() = 0;
