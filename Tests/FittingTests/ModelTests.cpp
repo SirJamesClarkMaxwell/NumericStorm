c05673fd83544fa1b72e5ac886420d475c2bdea0
ArnoXX <tbolz2112@gmail.com>
2024-02-19 19:31:48 +0100

    fixed tests

--- Tests/FittingTests/ModelTests.cpp
+++ Tests/FittingTests/ModelTests.cpp
@@ -39,14 +39,17 @@ std::unique_ptr<Data> gaussian(const std::vector<double>& arguments, const Param
 	calculateData.resize(arguments.size());
 	double A = parameters[0];
 	double mu = parameters[1];
-	double sigma = parameters[3];
-	double c = parameters[4];
+	double sigma = parameters[2];
+	double c = parameters[3];
+
+
 
 	for (int i = 0; i < arguments.size(); i++)
 	{
-		double updatedX = -(arguments[i] - mu);
-		calculateData[i] = A * exp(pow(updatedX, 2) / (2 * sigma)) + c;
+		double updatedX = (arguments[i] - mu);
+		calculateData[i] = A * exp(-pow(updatedX, 2) / (2 * sigma)) + c;
 	}
+
 	return std::make_unique<GaussianData>(arguments, calculateData);
 }
 //using GaussianFunction = 
@@ -55,9 +58,11 @@ struct TestingGaussianModel:public testing::Test
 	std::array<double, 4> parametersArray{ 2,1,2,-1 };
 	Parameters<4> initialParameters{ parametersArray };
 	
-	std::vector<double> arguments{ -2,- 1,0,1,2	};
-	std::vector<double> trueValues{ 17.97547167,4.436563657,1.568050833,1,1.568050833};
+	std::vector<double> arguments{ -2,-1,0,1,2	};
 	AdditionalParameters additionalParameters{};
+	std::unique_ptr<Data> realData = gaussian(arguments, initialParameters, additionalParameters);
+	//std::vector<double> trueValues{ -0.789201551,-0.264241118,0.557601566,1,0.557601566 };
+	std::vector<double> trueValues = (*realData).getValues();
 
 	std::function<std::unique_ptr<Data>(const std::vector<double>& arguments, const Parameters<4>& parameters, const AdditionalParameters& additionalParameters)> gaussianFunction = gaussian;
 
@@ -68,11 +73,9 @@ TEST_F(TestingGaussianModel, TestingSimpleGaussianModel)
 {
 	Model<4>* testedModel = new GaussianModel(gaussian);
 
-	std::unique_ptr<Data> testedData;
-
-	testedData = (*testedModel)(arguments, initialParameters, additionalParameters);
+	std::unique_ptr<Data> testedData = (*testedModel)(arguments, initialParameters, additionalParameters);
 	auto testedValues = (*testedData).getValues();
 	for (int i = 0; i < arguments.size(); i++)
-		EXPECT_DOUBLE_EQ(testedValues[i],trueValues[i],0.01);
+		EXPECT_DOUBLE_EQ(testedValues[i],trueValues[i]);
 };
 }
