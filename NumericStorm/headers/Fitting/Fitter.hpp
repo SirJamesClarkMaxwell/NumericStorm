5fcca39e12b7e35f0f96b07161efdf84f05fbdb6
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-20 10:13:44 +0100

    removed the shared pointer to model and error model in fitter removed the declaration of the setUp function in fitter changed

--- NumericStorm/headers/Fitting/Fitter.hpp
+++ NumericStorm/headers/Fitting/Fitter.hpp
@@ -12,13 +12,12 @@ public:
 	Fitter() {};// = default;
 	//virtual Parameters<parameter_size>* getFittedParameters() = 0;
 	virtual void minimize() = 0;
-	virtual void setUp(FitterSettings<parameter_size>* fitterSettings) = 0;
+
 protected:
 	Parameters<parameter_size> m_fittedParamters;
 	Parameters<parameter_size> m_proposedParameters;
 	FitterSettings<parameter_size>* m_fitterSettings;
-	std::shared_ptr<Model<parameter_size>> m_model;
-	std::shared_ptr<ErrorModel> m_errorModel;
+
 
 };
 
