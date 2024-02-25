03ff2b4b64aac6684f4e614ac7a7c06f51f13b8c
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-25 16:12:08 +0100

    fixed the typo in Data member varaibles

--- NumericStorm/headers/Fitting/Parameters.hpp
+++ NumericStorm/headers/Fitting/Parameters.hpp
@@ -11,37 +11,32 @@
 namespace NumericStorm {
 namespace Fitting {
 
-        //template <size_t parameter_size>
-        //class Model;
-
-        template <size_t parameter_size>
-        class Parameters {
-        public:
-            Parameters() {};
-            Parameters(std::array<double, parameter_size> parameters)
-                : m_parameters(parameters), m_error(-1)
-                /*, m_ModelSet(false), m_ErrorModelSet(false), m_errorModel(nullptr), m_model(nullptr)*/ {};
 
 
-            std::array<double, parameter_size> getParameters() { return m_parameters; };
-
-        double getError();
-        double& operator[](int index) noexcept
-        {
-            if (index >= 0 && static_cast<std::size_t>(index) < parameter_size)
-                return m_parameters[index];
-            return m_parameters[0];
-        }
-        const double& operator[](int index) const noexcept
-        {
-            if (index >= 0 && static_cast<std::size_t>(index) < parameter_size)
-                return m_parameters[index];
-            return m_parameters[0];
-        }
-        protected:
-            std::array<double, parameter_size> m_parameters;
-            double m_error;
-        };
+template <size_t parameter_size>
+class Parameters {
+public:
+    Parameters() {};
+    Parameters(std::array<double, parameter_size> parameters)
+        : m_parameters(parameters), m_error(-1){}
+    std::array<double, parameter_size> getParameters() { return m_parameters; }
+    double getError();
+    double& operator[](int index) noexcept
+    {
+        if (index >= 0 && static_cast<std::size_t>(index) < parameter_size)
+            return m_parameters[index];
+        return m_parameters[0];
+    }
+    const double& operator[](int index) const noexcept
+    {
+        if (index >= 0 && static_cast<std::size_t>(index) < parameter_size)
+            return m_parameters[index];
+        return m_parameters[0];
+    }
+    protected:
+        std::array<double, parameter_size> m_parameters;
+        double m_error;
+};
 }
 }
 
