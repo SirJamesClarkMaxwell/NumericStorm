5996ec796ef9f2e9e300da72ebaa528c005a6aa7
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-25 19:37:32 +0100

    changed template parameters

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/SimplexOperationFactory.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/SimplexOperationFactory.hpp
@@ -10,50 +10,51 @@
 
 namespace NumericStorm
 {
-	namespace Fitting
+namespace Fitting
+{
+template <size_t parameter_size>
+class SimplexOperationFactory
+{
+public:
+	SimplexOperationFactory(){};
+	void registerOperation(std::string operationName, ISimplexOperation<parameter_size> operation);
+	void unRegisterOperation(std::string operatioName); //throw NoAvailableFactoryException;
+	ISimplexOperation<parameter_size> createOperation(std::string name, SimplexOperationArguments additionalArguments); //throw NoAvailableFactoryException;
+
+private:
+	std::unordered_map<std::string, ISimplexOperation<parameter_size>> m_listOperation;
+};
+
+template <size_t parameter_size>
+void SimplexOperationFactory<parameter_size>::registerOperation(std::string operationName,
+															 ISimplexOperation<parameter_size>operation)
+{
+	m_listOperation[operationName] = operation;
+}
+
+template <size_t parameter_size>
+void SimplexOperationFactory<parameter_size>::unRegisterOperation(std::string operatioName)
+{
+	if (!(m_listOperation.erase(operatioName)))
+		throw NoAvailableFactoryException(operatioName);
+}
+
+template <size_t parameter_size>
+ISimplexOperation<parameter_size> SimplexOperationFactory<parameter_size>::createOperation(std::string operationName, SimplexOperationArguments additionalArguments)
+{
+	#if DEBUG
+	if (true)
 	{
-		template <size_t figure_size>
-		class SimplexOperationFactory
-		{
-		public:
-			SimplexOperationFactory(){};
-			void registerOperation(std::string operationName, ISimplexOperation<figure_size> operation);
-			void unRegisterOperation(std::string operatioName) throw NoAvailableFactoryException;
-			ISimplexOperation<figure_size> createOperation(std::string name, SimplexOperationArguments additionalArguments) throw NoAvailableFactoryException;
-
-		private:
-			std::unordered_map<std::string, ISimplexOperation<figure_size>> m_listOperation;
-		};
-
-		template <size_t figure_size>
-		void SimplexOperationFactory<figure_size>::registerOperation(std::string operationName,
-																	 ISimplexOperation<figure_size> operation)
-		{
-			m_listOperation[operationName] = operation;
-		}
-
-		template <size_t figure_size>
-		void SimplexOperationFactory<figure_size>::unRegisterOperation(std::string operatioName)
-		{
-			if !(m_listOperation.erase(operatioName))
-				throw NoAvailableFactoryException(operatioName);
-		}
-
-		template <size_t figure_size>
-		ISimplexOperation<figure_size> SimplexOperationFactory<figure_size>::createOperation(std::string operationName, SimplexOperationArguments additionalArguments)
-		{
-#if DEBUG
-			if (true)
-			{
-				ISimplexOperation<figure_size> operationObject = m_listOperation[operationName];
-				auto &toReturn = operationObject(additionalArguments);
-				return toReturn;
-			}
-			else
-				throw NoAvailableFactoryException(operationName)
-#endif
-					return m_listOperation[operationName](additionalArguments);
-		}
-
+		ISimplexOperation<parameter_size> operationObject = m_listOperation[operationName];
+		auto& toReturn = operationObject(additionalArguments);
+		return toReturn;
 	}
+	else
+		throw NoAvailableFactoryException(operationName)
+#endif
+		auto& toReturn = m_listOperation[operationName];// (additionalArguments);
+	return toReturn;
+}
+
+}
 }
