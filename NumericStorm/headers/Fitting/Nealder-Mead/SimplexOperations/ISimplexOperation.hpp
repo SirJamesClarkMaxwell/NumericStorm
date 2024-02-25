2adf5cac7847aa742c8a6d6950cb32d522647341
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-25 19:18:42 +0100

    updated interface, now i will re-implement this interface in derived classes

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/ISimplexOperation.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/ISimplexOperation.hpp
@@ -10,12 +10,12 @@ template<size_t figure_size>
 class ISimplexOperation
 {
 public:
-    ISimplexOperation(const std::string& name, const SimplexFigure<figure_size>& simplexFigure)
-        :m_operationName(name), m_simplexFigure(simplexFigure){};
-    //virtual SimplexFigure<figure_size> operation(const SimplexOperationArguments& arguments) = 0;
-    virtual SimplexFigure<figure_size>operator ()(const SimplexOperationArguments& arguments) = 0;
+    ISimplexOperation(const std::string& name, const SimplexOperationArguments& arguments)
+        :m_operationName(name), m_arguments(arguments){};
+
+    virtual SimplexFigure<figure_size>operator ()() = 0;
 protected:
-    SimplexFigure<figure_size> m_simplexFigure;
+    SimplexOperationArguments m_arguments;
     std::string m_operationName;
     
 };
