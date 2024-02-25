7f04c66a003f72136dc8eddc4e6cf8dee7f4ded4
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-25 19:10:00 +0100

    updated todo in this file

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/Reflection.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/Reflection.hpp
@@ -2,42 +2,46 @@
 #include "ISimplexOperation.hpp"
 #include "ReflectionOperationArguments.hpp"
 #include "../SimplexFigure.hpp"
-namespace NumericStorm {
-namespace Fitting 
+
+// TODO: redefine interface of Reflection
+namespace NumericStorm
 {
-template<size_t parameter_size>
-class Reflection :public ISimplexOperation<parameter_size> 
-{
+	namespace Fitting
+	{
+		template <size_t parameter_size>
+		class Reflection : public ISimplexOperation<parameter_size>
+		{
 
-public:
-	Reflection(const std::string& name, 
-		const SimplexFigure<parameter_size>& simplexFigure)
-		:ISimplexOperation<parameter_size>(name, simplexFigure) {};
+		public:
+			Reflection(const std::string &name,
+					   const SimplexFigure<parameter_size> &simplexFigure)
+				: ISimplexOperation<parameter_size>(name, simplexFigure){};
 
-	SimplexFigure<parameter_size> operator()  (const ReflectionOperationArguments<parameter_size>& arguments) override;
-protected:
-	std::string m_operationName = "reflection";
-};
+			SimplexFigure<parameter_size> operator()(const ReflectionOperationArguments<parameter_size> &arguments) override;
 
-template<size_t parameter_size>
-SimplexFigure<parameter_size> Reflection<parameter_size>::operator() (const ReflectionOperationArguments<parameter_size>& arguments)
-{
-	SimplexFigure<parameter_size> reflectedFigure(this->m_simplexFigure);
-	
-	double alpha = arguments.getFactor();
-	SimplexPoint<parameter_size > centroid = reflectedFigure.getCentroid();
-	SimplexPoint<parameter_size > pointToReflectArround(centroid);
-	#if DEBUG
-		auto difference = centroid - reflectedFigure[0];
-		auto multiplicated = difference * alpha;	
-		pointToReflectArround += multiplicated;
-	#else if REALASE
-	pointToReflectArround += (centroid - reflectedFigure[0]) * alpha;
-	#endif
+		protected:
+			std::string m_operationName = "reflection";
+		};
 
-		reflectedFigure[0] = pointToReflectArround;
-		return reflectedFigure;
-}
+		template <size_t parameter_size>
+		SimplexFigure<parameter_size> Reflection<parameter_size>::operator()(const ReflectionOperationArguments<parameter_size> &arguments)
+		{
+			SimplexFigure<parameter_size> reflectedFigure(this->m_simplexFigure);
 
-}
+			double alpha = arguments.getFactor();
+			SimplexPoint<parameter_size> centroid = reflectedFigure.getCentroid();
+			SimplexPoint<parameter_size> pointToReflectArround(centroid);
+#if DEBUG
+			auto difference = centroid - reflectedFigure[0];
+			auto multiplicated = difference * alpha;
+			pointToReflectArround += multiplicated;
+#else if REALASE
+			pointToReflectArround += (centroid - reflectedFigure[0]) * alpha;
+#endif
+
+			reflectedFigure[0] = pointToReflectArround;
+			return reflectedFigure;
+		}
+
+	}
 }
--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/SimplexOperationFactory.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/SimplexOperationFactory.hpp
@@ -5,50 +5,55 @@
 #include "SimplexOperationArguments.hpp"
 #include "NoAvailableFactoryException.hpp"
 
+// TODO: we need to redefine this methods, operator should receive the SimplexFigure argument and as a member variable should hold the SimplexOperationArgument, and this should be a probably a sharedPointer
+// TODO: redefine interface of SimplexOperation
+
 namespace NumericStorm
 {
-namespace Fitting
-{
-template <size_t figure_size>
-class SimplexOperationFactory 
-{
-public:
-	SimplexOperationFactory() {};
-	void registerOperation(std::string operationName, ISimplexOperation<figure_size> operation);
-	void unRegisterOperation(std::string operatioName) throw NoAvailableFactoryException;
-	ISimplexOperation<figure_size> createOperation(std::string name,SimplexOperationArguments additionalArguments) throw NoAvailableFactoryException;
-private:
-	std::unordered_map<std::string, ISimplexOperation<figure_size>> m_listOperation;
-};
-
-
-template<size_t figure_size>
-void SimplexOperationFactory<figure_size>::registerOperation(std::string operationName, 
-	ISimplexOperation<figure_size> operation)
-{m_listOperation[operationName] = operation;}
-
-template<size_t figure_size>
-void SimplexOperationFactory<figure_size>::unRegisterOperation(std::string operatioName) 
-{
-	if !(m_listOperation.erase(operatioName))
-		throw NoAvailableFactoryException(operatioName);
-}
-
-template<size_t figure_size>
-ISimplexOperation<figure_size> SimplexOperationFactory<figure_size>::createOperation(std::string operationName,SimplexOperationArguments additionalArguments)
-{
-	#if DEBUG
-	if (true) 
+	namespace Fitting
 	{
-		ISimplexOperation<figure_size> operationObject = m_listOperation[operationName];
-		auto& toReturn = operationObject(additionalArguments);
-		return toReturn;
-	}
-	else 
-		throw NoAvailableFactoryException(operationName)
-	#endif
-	return m_listOperation[operationName](additionalArguments);
-}
+		template <size_t figure_size>
+		class SimplexOperationFactory
+		{
+		public:
+			SimplexOperationFactory(){};
+			void registerOperation(std::string operationName, ISimplexOperation<figure_size> operation);
+			void unRegisterOperation(std::string operatioName) throw NoAvailableFactoryException;
+			ISimplexOperation<figure_size> createOperation(std::string name, SimplexOperationArguments additionalArguments) throw NoAvailableFactoryException;
 
-}
+		private:
+			std::unordered_map<std::string, ISimplexOperation<figure_size>> m_listOperation;
+		};
+
+		template <size_t figure_size>
+		void SimplexOperationFactory<figure_size>::registerOperation(std::string operationName,
+																	 ISimplexOperation<figure_size> operation)
+		{
+			m_listOperation[operationName] = operation;
+		}
+
+		template <size_t figure_size>
+		void SimplexOperationFactory<figure_size>::unRegisterOperation(std::string operatioName)
+		{
+			if !(m_listOperation.erase(operatioName))
+				throw NoAvailableFactoryException(operatioName);
+		}
+
+		template <size_t figure_size>
+		ISimplexOperation<figure_size> SimplexOperationFactory<figure_size>::createOperation(std::string operationName, SimplexOperationArguments additionalArguments)
+		{
+#if DEBUG
+			if (true)
+			{
+				ISimplexOperation<figure_size> operationObject = m_listOperation[operationName];
+				auto &toReturn = operationObject(additionalArguments);
+				return toReturn;
+			}
+			else
+				throw NoAvailableFactoryException(operationName)
+#endif
+					return m_listOperation[operationName](additionalArguments);
+		}
+
+	}
 }
