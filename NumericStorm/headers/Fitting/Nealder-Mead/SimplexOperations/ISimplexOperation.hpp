8400155b9b1a7dbbc2c200a6e2a53532e7ad463e
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-02-26 10:44:34 +0100

    added methods for updating factory settings
    i am not sure that this should be in the same class, maybe we need to create a dedicated class for updating this SimplexOperations objects

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/ISimplexOperation.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/ISimplexOperation.hpp
@@ -10,12 +10,13 @@ template<size_t parameter_size>
 class ISimplexOperation
 {
 private:
-    ISimplexOperation(const std::string& name, const SimplexOperationSettigns& arguments)
-        :m_operationName(name), m_settings(arguments){};
+    ISimplexOperation(const std::string& operationName, const SimplexOperationSettigns& arguments)
+        :m_operationName(operationName), m_settings(arguments){};
 
 public:
     virtual SimplexFigure<parameter_size>operator ()(const SimplexFigure<parameter_size>&simplexFigure) = 0;
-    void updateSettigns(const SimplexOperationSettigns& newArguments);
+    void updateSettigns(const SimplexOperationSettigns& newSettings)
+        {m_settings = newSettings;}
 protected:
     SimplexOperationSettigns m_settings;
     std::string m_operationName;
--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/SimplexOperationFactory.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexOperations/SimplexOperationFactory.hpp
@@ -20,8 +20,9 @@ public:
 	SimplexOperationFactory() {};
 	void registerOperation(std::string operationName, ISimplexOperation_ptr operation);
 	void unRegisterOperation(std::string operatioName) throw (NoAvailableFactoryException);
-	SimplexFigure<parameter_size> createOperation(std::string name, const SimplexFigure<parameter_size> simplexFigure) throw (NoAvailableFactoryException);
+	SimplexFigure<parameter_size> createOperation(std::string operationName, const SimplexFigure<parameter_size> simplexFigure) throw (NoAvailableFactoryException);
 	void updateOperationSettings(std::string operationName, SimplexOperationSettigns newSettings)throw (NoAvailableFactoryException);
+	void updateOperationsSettings(std::vector<std::string> operationNames, std::vector<SimplexOperationSettigns> newSettings)throw (NoAvailableFactoryException);
 
 private:
 	std::unordered_map<std::string, ISimplexOperation_ptr> m_listOperation;
@@ -39,14 +40,40 @@ void SimplexOperationFactory<parameter_size>::unRegisterOperation(std::string op
 };
 
 template <size_t parameter_size>
-SimplexFigure<parameter_size> SimplexOperationFactory<parameter_size>::createOperation(std::string name, const SimplexFigure<parameter_size> simplexFigure)
+SimplexFigure<parameter_size> SimplexOperationFactory<parameter_size>::createOperation(std::string operationName, const SimplexFigure<parameter_size> simplexFigure)
 {
-	ISimplexOperation_ptr simplexOperation = m_listOperation.find(name);
+	ISimplexOperation_ptr simplexOperation = m_listOperation.find(operationName);
 
 	if (simplexOperation != m_listOperation.end())
 		return  simplexOperation->second(simplexFigure);
 	else
-		throw NoAvailableFactoryException(name);
+		throw NoAvailableFactoryException(operationName);
+}
+template<size_t parameter_size>
+void SimplexOperationFactory<parameter_size>::updateOperationSettings(std::string operationName, SimplexOperationSettigns newSettings) throw(NoAvailableFactoryException)
+{
+	ISimplexOperation_ptr simplexOperation = m_listOperation.find(operationName);
+
+	if (simplexOperation != m_listOperation.end())
+		return  simplexOperation->second.updateSettings(newSettings);
+	else
+		throw NoAvailableFactoryException(operationName);
 };
+
+template <size_t parameter_size>
+void SimplexOperationFactory<parameter_size>::updateOperationsSettings(std::vector<std::string> operationNames, std::vector<SimplexOperationSettigns> newSettings) 
+{
+	for (int i = 0; i < operationNames.size(); i++)
+	{
+		ISimplexOperation_ptr& simplexOperation = m_listOperation.find(operationNames[i]);
+
+		if (simplexOperation != m_listOperation.end())
+			simplexOperation->second.updateSettings(newSettings[i]);
+		else
+			throw NoAvailableFactoryException(operationNames[i]);
+	}
+
+}
+
 }
 }
