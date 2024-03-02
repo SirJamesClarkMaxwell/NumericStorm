42b257a80804ab88a46c511b14f25ff12c74185d
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-03-02 14:04:13 +0100

    changed the implementation of the Model class to be a concreate class, not an abstract we need only one level of abstraction

--- NumericStorm/headers/Fitting/Model.hpp
+++ NumericStorm/headers/Fitting/Model.hpp
@@ -12,18 +12,18 @@ template <size_t parameter_size>
 class Model {
 public:
     Model(std::function<std::unique_ptr<Data>(const std::vector<double>& arguments, const Parameters<parameter_size>& parameters,
-        const AdditionalParameters& additionalParameters)> model)
+        const AdditionalParameters& additionalParameters())> model)
         :m_model(model) {};
-    virtual ~Model() = 0;
-    virtual std::unique_ptr<Data> operator()(const std::vector<double>& arguments,const Parameters<parameter_size>& parameters,
-        const AdditionalParameters& additionalParameters) = 0;
+    std::unique_ptr<Data> operator()(const std::vector<double>& arguments,const Parameters<parameter_size>& parameters,
+        const AdditionalParameters& additionalParameters()) ;
 
 protected:
     std::function<std::unique_ptr<Data> (const std::vector<double>& arguments,const Parameters<parameter_size>& parameters,
-        const AdditionalParameters& additionalParameters)> m_model;
+        const AdditionalParameters& additionalParameters())> m_model;
 };
 
 template<size_t parameter_size>
-Model<parameter_size>::~Model() {};
+std::unique_ptr<Data> Model<parameter_size>::operator()(const std::vector<double>& arguments, const Parameters<parameter_size>& parameters, const AdditionalParameters& additionalParameters())
+    {return m_model(arguments,parameters,additionalParameters);}
 }
 }
