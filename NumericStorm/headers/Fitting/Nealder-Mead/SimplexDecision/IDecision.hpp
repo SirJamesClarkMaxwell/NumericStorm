9d3b677a5eef4d9fcdac41857382ddd82f04a48c
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-03-03 19:46:50 +0100

    added the not full implementation of the DecisionStrategy

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexDecision/IDecision.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexDecision/IDecision.hpp
@@ -1,17 +1,21 @@
 #include <unordered_map>
 #include <string>
 
-namespace NumericStorm 
+#include "../SimplexFigure.hpp"
+
+namespace NumericStorm
 {
-namespace Fitting 
-{
-class IStrategy 
-{
+    namespace Fitting
+    {
 
+        template <size_t parameter_size>
+        class IDecision
+        {
+        public:
+            IDecision() = default;
+            virtual ~IDecision() = default;
+            virtual SimplexFigure<parameter_size> makeDecision(std::array<SimplexFigure<parameter_size>, parameter_size + 1> simplexFigures) = 0;
+        };
 
-
-};
-
-
-}
+    }
 }
