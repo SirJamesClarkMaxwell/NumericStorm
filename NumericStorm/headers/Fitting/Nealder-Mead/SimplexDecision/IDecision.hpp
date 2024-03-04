5aed424df864fead53ef43ea8ae6f8a34aabe5a4
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-03-04 18:49:00 +0100

    added my thought about the makeDecision() method and extending his functionality

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexDecision/IDecision.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexDecision/IDecision.hpp
@@ -15,6 +15,9 @@ namespace NumericStorm
 			IDecision() = default;
 			virtual ~IDecision() = default;
 			virtual SimplexFigure<parameter_size> makeDecision(const std::vector<SimplexFigure<parameter_size>> &simplexFigures) = 0;
+			//! probably makeDecision should have an optional argument and there we could store the points from annealing
+			//! or we could create another method with one additional argument, and later in some way set things up by another method
+			//! but this will be quite slow because in that case we will need to set things up in every iteration of the algorithm
 		};
 
 	}
