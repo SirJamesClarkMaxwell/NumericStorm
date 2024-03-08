1e3ff2d4234dd8f3dd0870807c9e6bcf972f1090
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-03-09 00:37:31 +0100

    maked code cleaner and andded comparasing adress of model and errorModel

--- Tests/FittingTests/FittingTestsSetUp.hpp
+++ Tests/FittingTests/FittingTestsSetUp.hpp
@@ -5,14 +5,14 @@ using namespace NumericStorm::Fitting;
 class GaussianData : public Data
 {
 public:
-    GaussianData(const std::vector<double> &arguments, const std::vector<double> &values)
+    GaussianData(const std::vector<double>& arguments, const std::vector<double>& values)
         : Data(arguments, values) {}
-    virtual ~GaussianData(){};
+    virtual ~GaussianData() {};
     std::vector<double> getArguments() override { return this->m_argumets; };
     std::vector<double> getValues() override { return this->m_values; };
 };
 
-std::unique_ptr<Data> gaussian(const std::vector<double> &arguments, const Parameters<4> &parameters, const AdditionalParameters &additionalParameters)
+std::unique_ptr<Data> gaussian(const std::vector<double>& arguments, const Parameters<4>& parameters, const AdditionalParameters& additionalParameters)
 {
     std::vector<double> calculateData;
     calculateData.resize(arguments.size());
@@ -25,7 +25,7 @@ std::unique_ptr<Data> gaussian(const std::vector<double> &arguments, const Param
     }
     return std::make_unique<GaussianData>(arguments, calculateData);
 }
-double chi2ErrorModelFunction(const std::shared_ptr<Data> &referenceData, const std::shared_ptr<Data> &evaluatedData)
+double chi2ErrorModelFunction(const std::shared_ptr<Data>& referenceData, const std::shared_ptr<Data>& evaluatedData)
 {
     std::vector<double> referenceValues = referenceData->getValues();
     std::vector<double> evaluatedValues = evaluatedData->getValues();
@@ -38,19 +38,40 @@ double chi2ErrorModelFunction(const std::shared_ptr<Data> &referenceData, const 
 class UsefullyObjects
 {
 public:
-    UsefullyObjects(){};
-    std::array<double, 4> referencedArray{2, 1, 2, -1};
-    std::array<double, 4> evaluatedArray{1, 1, 1, -1};
+    UsefullyObjects()
+    {
+        std::array<double, 4> referencedArray{ 2, 1, 2, -1 };
+        std::array<double, 4> evaluatedArray{ 1, 1, 1, -1 };
+        Parameters<4> referencedParameters{ referencedArray };
+        Parameters<4> evaluatedParameters{ evaluatedArray };
+        std::vector<double> arguments{ -2, -1, 0, 1, 2 };
+        std::function<double(const std::shared_ptr<Data>& referenceData, const std::shared_ptr<Data>& evaluatedData)> chi2Model = chi2ErrorModelFunction;
+        std::function<std::unique_ptr<Data>(const std::vector<double>& arguments, const Parameters<4>& parameters, const AdditionalParameters& additionalParameters)> gaussianFunction = gaussian;
 
-    Parameters<4> referencedParameters{referencedArray};
-    Parameters<4> evaluatedParameters{evaluatedArray};
 
-    std::vector<double> arguments{-2, -1, 0, 1, 2};
+        referencedData = gaussian(arguments, referencedParameters, additionalParameters);
+        evaluatedData = gaussian(arguments, evaluatedParameters, additionalParameters);
+        //gaussianModel = gaussianModel;
+        //chi2ErrorModel = chi2Model;
+        sharedPtrModel = std::make_shared<Model<4>>(Model<4>(gaussian));
+        sharedPtrErrorModel = std::make_shared<ErrorModel>(ErrorModel(chi2ErrorModelFunction));
+
+        trueError = chi2ErrorModelFunction(referencedData, evaluatedData);
+    };
+    std::array<double, 4> referencedArray{ 2, 1, 2, -1 };
+    std::array<double, 4> evaluatedArray{ 1, 1, 1, -1 };
+
+    Parameters<4> referencedParameters{ referencedArray };
+    Parameters<4> evaluatedParameters{ evaluatedArray };
+
+    std::vector<double> arguments{ -2, -1, 0, 1, 2 };
     AdditionalParameters additionalParameters{};
-    std::shared_ptr<Data> referencedData = gaussian(arguments, referencedParameters, additionalParameters);
-    std::shared_ptr<Data> evaluatedData = gaussian(arguments, evaluatedParameters, additionalParameters);
+    std::shared_ptr<Data> referencedData;
+    std::shared_ptr<Data> evaluatedData;
 
-    std::function<double(const std::shared_ptr<Data> &referenceData, const std::shared_ptr<Data> &evaluatedData)> chi2Model = chi2ErrorModelFunction;
-    std::function<std::unique_ptr<Data>(const std::vector<double> &arguments, const Parameters<4> &parameters, const AdditionalParameters &additionalParameters)> gaussianFunction = gaussian;
-    double trueError = chi2ErrorModelFunction(referencedData, evaluatedData);
+    double trueError;
+    //Model<4> gaussianModel;
+    //ErrorModel chi2ErrorModel;
+    std::shared_ptr<Model<4>> sharedPtrModel;
+    std::shared_ptr<ErrorModel> sharedPtrErrorModel;
 };
