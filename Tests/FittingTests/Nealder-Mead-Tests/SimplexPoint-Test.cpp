f019bc12def2205567b9ac4382e16faff41167dd
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-03-08 22:04:50 +0100

    i have found another problem

--- Tests/FittingTests/Nealder-Mead-Tests/SimplexPoint-Test.cpp
+++ Tests/FittingTests/Nealder-Mead-Tests/SimplexPoint-Test.cpp
@@ -4,26 +4,26 @@ using namespace NumericStorm::Fitting;
 
 namespace TestingSimplexPointOperations
 {
-    //* Testowanie funkcji setUp
-    struct TestSetUpFunction : public ::testing::Test
-    {
-        UsefullyObjects usefullyObjects;
-        SimplexPoint<4> testingSimplexPoint{usefullyObjects.referencedArray};
-        testingSimplexPoint.setModel(usefullyObjects.gaussian, usefullyObjects.chi2Model);
-    };
+//* Testowanie funkcji setUp
+struct TestSetUpFunction : public ::testing::Test
+{
+    UsefullyObjects usefullyObjects;
+    SimplexPoint<4> testingSimplexPoint{ usefullyObjects.referencedArray };
+    testingSimplexPoint.setModel(usefullyObjects.gaussian, usefullyObjects.chi2Model);
+};
 
-    TEST_F(TestSetUpFunction, settingModel)
-    {
-        EXPECT_TRUE(tetingSimplexPoint.);
-        EXPECT_TRUE(tetingSimplexPoint);
-    }
-    //* testowanie rzucania błędów o nieustawieniu modelu i błedu modelu
+TEST_F(TestSetUpFunction, settingModel)
+{
+    EXPECT_TRUE(tetingSimplexPoint.modelIsSet());
+    EXPECT_TRUE(tetingSimplexPoint.errorModelIsSet());
+}
+//* testowanie rzucania błędów o nieustawieniu modelu i błedu modelu
 
-    //* Testowanie funkcji calculateData
-    //* Testowanie funkcji calculateError
+//* Testowanie funkcji calculateData
+//* Testowanie funkcji calculateError
 
-    //! inny plik
-    //* testowanie operatorów porównania
-    //* testowanie konstruktora kopiującego
+//! inny plik
+//* testowanie operatorów porównania
+//* testowanie konstruktora kopiującego
 
 }
