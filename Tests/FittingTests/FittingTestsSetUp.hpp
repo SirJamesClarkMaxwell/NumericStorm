25adc210fca164d15b1233fa5526b5ae5dccb16a
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-03-09 19:26:10 +0100

    added the tests for calculating the data by derived class

--- Tests/FittingTests/FittingTestsSetUp.hpp
+++ Tests/FittingTests/FittingTestsSetUp.hpp
@@ -34,27 +34,44 @@ double chi2ErrorModelFunction(const std::shared_ptr<Data>& referenceData, const 
         chi2 += pow((referenceValues[i] - evaluatedValues[i]), 2);
     return chi2;
 }
-class Chi2ErrorModel : public ErrorModel
-{
-    //TODO: make function the actual function static and remove parameter form constructor
+class Chi2ErrorModel : public ErrorModel {
 public:
-    Chi2ErrorModel(std::function<double(const std::shared_ptr<Data>&, const std::shared_ptr<Data>&)> chi2Model)
-        : ErrorModel(chi2Model) {}
-    double operator()(const std::shared_ptr<Data>& referenceData, const std::shared_ptr<Data>& evaluatedData)
-    {
-        return this->m_errorModel(referenceData, evaluatedData);
+    // No need for constructor parameters
+    Chi2ErrorModel() : ErrorModel(chi2ErrorModelFunction) {}
+
+    static double chi2ErrorModelFunction(const std::shared_ptr<Data>& referenceData, const std::shared_ptr<Data>& evaluatedData) {
+        std::vector<double> referenceValues = referenceData->getValues();
+        std::vector<double> evaluatedValues = evaluatedData->getValues();
+        double chi2 = 0;
+        for (size_t i = 0; i < referenceValues.size(); i++)
+            chi2 += pow((referenceValues[i] - evaluatedValues[i]), 2);
+        return chi2;
+    }
+
+    double operator()(const std::shared_ptr<Data>& referenceData, const std::shared_ptr<Data>& evaluatedData) {
+        return chi2ErrorModelFunction(referenceData, evaluatedData); // Call the static function
     };
 };
-
-class GaussianModel : public Model<4>
-{
-    //TODO: make function the actual function static and remove parameter form constructor
+class GaussianModel : public Model<4> {
 public:
-    GaussianModel(std::function<std::unique_ptr<Data>(const std::vector<double>&, const Parameters<4>&, const AdditionalParameters&)> gaussian)
-        :Model<4>(gaussian) {};
-    std::unique_ptr<Data> operator()(const std::vector<double>& arguments, const Parameters<4>& parameters,
-        const AdditionalParameters& additionalParameters) {
-        return this->m_model(arguments, parameters, additionalParameters);
+    // No need for constructor parameters
+    GaussianModel() : Model<4>(gaussianFunction) {};
+
+    static std::unique_ptr<Data> gaussianFunction(const std::vector<double>& arguments, const Parameters<4>& parameters, const AdditionalParameters& additionalParameters) 
+    {
+        std::vector<double> calculateData;
+        calculateData.resize(arguments.size());
+        double A = parameters[0], mu = parameters[1], sigma = parameters[2], c = parameters[3];
+
+        for (int i = 0; i < arguments.size(); i++)
+        {
+            double updatedX = (arguments[i] - mu);
+            calculateData[i] = A * exp(-pow(updatedX, 2) / (2 * sigma)) + c;
+        }
+        return std::make_unique<GaussianData>(arguments, calculateData);
+    }
+    std::unique_ptr<Data> operator()(const std::vector<double>& arguments, const Parameters<4>& parameters, const AdditionalParameters& additionalParameters) {
+        return gaussianFunction(arguments, parameters, additionalParameters); // Call the static function
     };
 };
 class UsefullyObjects
@@ -78,8 +95,8 @@ public:
 
         trueError = chi2ErrorModelFunction(referencedData, evaluatedData);
 
-        gaussianModel = std::make_shared<GaussianModel>(gaussianFunction);
-        chi2ErrorModel = std::make_shared<Chi2ErrorModel>(chi2Model);
+        gaussianModel = std::make_shared<GaussianModel>();
+        chi2ErrorModel = std::make_shared<Chi2ErrorModel>();
     };
     std::array<double, 4> referencedArray{ 2, 1, 2, -1 };
     std::array<double, 4> evaluatedArray{ 1, 1, 1, -1 };
