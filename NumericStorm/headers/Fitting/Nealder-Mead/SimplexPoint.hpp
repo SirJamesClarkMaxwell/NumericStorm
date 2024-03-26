5a93fe01e72b92911cb8728ac8e6191cd6fe22cc
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-03-26 12:05:23 +0100

    same message

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexPoint.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexPoint.hpp
@@ -8,42 +8,40 @@
 
 
 namespace NumericStorm
-
 {
 namespace Fitting
 {
-
-
-template <std::size_t parameter_size>
-class SimplexPoint : public Parameters<parameter_size> {
-	//TODO remove deriving from parameters class
+template <std::size_t parameter_size, class AuxilaryParameters = AdditionalParameters>
+class SimplexPoint {
 public:
 
+	SimplexPoint(const std::vector<double> arguments, const std::array<double, parameter_size>& parameters)
+		: Parameters<parameter_size, AuxilaryParameters>{ parameters }, m_data{ std::make_unique<Data>(arguments) } {};
 	SimplexPoint() = default;
-	SimplexPoint(const SimplexPoint<parameter_size>&) = default;
-	SimplexPoint(SimplexPoint<parameter_size>&&) = default;
-	SimplexPoint<parameter_size>& operator=(const SimplexPoint<parameter_size>&) = default;
-	SimplexPoint<parameter_size>& operator=(SimplexPoint<parameter_size>&&) = default;
+	SimplexPoint(const SimplexPoint<parameter_size, AuxilaryParameters>&) = default;
+	SimplexPoint(SimplexPoint<parameter_size, AuxilaryParameters>&&) = default;
+	SimplexPoint<parameter_size, AuxilaryParameters>& operator=(const SimplexPoint<parameter_size, AuxilaryParameters>&) = default;
+	SimplexPoint<parameter_size, AuxilaryParameters>& operator=(SimplexPoint<parameter_size, AuxilaryParameters>&&) = default;
 
 	virtual ~SimplexPoint() = default;
 
-	SimplexPoint(const Parameters<parameter_size>& params) : Parameters<parameter_size>(params), m_data{ std::make_unique<Data>() } {}
-	SimplexPoint(Parameters<parameter_size>&& params) : Parameters<parameter_size>(params), m_data{ std::make_unique<Data>() } {}
-
-
-	SimplexPoint(const std::vector<double> arguments, const std::array<double, parameter_size>& parameters)
-		: Parameters<parameter_size>{ parameters }, m_data{ std::make_unique<Data>(arguments) } {};
-
-
-
-	const Data& getData() const { return *(m_data); }
-	Data& getData() { m_data_valid = true; return *(m_data); }
-
-
+	SimplexPoint(const Parameters<parameter_size, AuxilaryParameters>& params) : Parameters<parameter_size, AuxilaryParameters>(params), m_data{ std::make_unique<Data>() } {}
+	SimplexPoint(Parameters<parameter_size, AuxilaryParameters>&& params) : Parameters<parameter_size, AuxilaryParameters>(params), m_data{ std::make_unique<Data>() } {}
 	bool isDataValid() const { return m_data_valid; }
 
+private:
+	Parameters<parameter_size, AuxilaryParameters> m_parameters{};
+	bool m_data_valid{ false };
+	//NOTE I am not sure that storing a copy of input arguments in each simplex point is the best approach, might change later
+	//NOTE we could store a reference into the data defined in the simplexFigure, or somewhere else maybe in FitterSettings or in Fitter
+	std::unique_ptr<Data> m_data{ nullptr };
+	std::shared_ptr<Data> m_referenceData{ nullptr };
+	double m_error{ -1 };
+	Model<parameter_size, AuxilaryParameters>* m_model{ nullptr };
+	ErrorModel* m_errorModel{ nullptr };
 
-	bool operator ==(const SimplexPoint<parameter_size>& other) const
+public:
+	bool operator ==(const SimplexPoint<parameter_size, AuxilaryParameters>& other) const
 	{
 		return this->m_parameters.getParameters() == other.getParameters();
 	}
@@ -51,93 +49,64 @@ public:
 	{
 		return this->m_parameters.getParameters() == other;
 	};
-	bool operator <=> (const SimplexPoint<parameter_size>& other) const
+	bool operator <=> (const SimplexPoint<parameter_size, AuxilaryParameters>& other) const
 	{
 		return this->m_error <=> other.m_error;
 	}
 
 	virtual double& operator[](int index) override
 	{
-		//overloading to add data invalidation
 		m_data_valid = false;
-		//instead of rewriting the entire operator we add functionality and call operator of the base class
-		//BUG here we will have a bug if we remove the Parameters dependency as a base class
-		//TODO make it as a member variable and return m_parameters[index]
-		return Parameters<parameter_size>::operator[](index);
+		//todo add model, error model, and recalculating the data after change of parameters
+		return m_parameters[index];
 	}
 
-	//NOTE i am not sure that we need all of these arithmetics operators, i would remove *, / ,*=, /= for SimplexPoint
-	SimplexPoint<parameter_size>& operator+=(const SimplexPoint<parameter_size>& other) {
+	SimplexPoint<parameter_size, AuxilaryParameters>& operator+=(const SimplexPoint<parameter_size, AuxilaryParameters>& other) {
 		for (std::size_t i = 0; i < parameter_size; ++i)
 			this->operator[](i) += other[i];
 		return *this;
 	}
-	SimplexPoint<parameter_size>& operator-=(const SimplexPoint<parameter_size>& other) {
+	SimplexPoint<parameter_size, AuxilaryParameters>& operator-=(const SimplexPoint<parameter_size, AuxilaryParameters>& other) {
 		for (std::size_t i = 0; i < parameter_size; ++i)
 			this->operator[](i) -= other[i];
 		return *this;
 	}
-	SimplexPoint<parameter_size>& operator*=(const  SimplexPoint<parameter_size>& other) {
-		for (std::size_t i = 0; i < parameter_size; ++i)
-			this->operator[](i) *= other[i];
-		return *this;
-	}
-	SimplexPoint<parameter_size>& operator/=(const  SimplexPoint<parameter_size>& other) {
-		for (std::size_t i = 0; i < parameter_size; ++i)
-			this->operator[](i) /= other[i];
-		return *this;
-	}
 
-	SimplexPoint<parameter_size>& operator*=(double scalar) {
+	SimplexPoint<parameter_size, AuxilaryParameters>& operator*=(double scalar) {
 		for (auto& param : this->m_parameters) {
 			param *= scalar;
 		}
 		return *this;
 	}
-	SimplexPoint<parameter_size>& operator/=(double scalar) {
+	SimplexPoint<parameter_size, AuxilaryParameters>& operator/=(double scalar) {
 		for (auto& param : this->m_parameters) {
 			param /= scalar;
 		}
 		return *this;
 	}
 
-	SimplexPoint<parameter_size> operator+(const  SimplexPoint<parameter_size>& other) const {
-		SimplexPoint<parameter_size> result = *this;
+	SimplexPoint<parameter_size, AuxilaryParameters> operator+(const  SimplexPoint<parameter_size, AuxilaryParameters>& other) const {
+		SimplexPoint<parameter_size, AuxilaryParameters> result = *this;
 		result += other;
 		return result;
 	}
-	SimplexPoint<parameter_size> operator-(const  SimplexPoint<parameter_size>& other) const {
-		SimplexPoint<parameter_size> result = *this;
+	SimplexPoint<parameter_size, AuxilaryParameters> operator-(const  SimplexPoint<parameter_size, AuxilaryParameters>& other) const {
+		SimplexPoint<parameter_size, AuxilaryParameters> result = *this;
 		result -= other;
 		return result;
 	}
-	SimplexPoint<parameter_size> operator*(const  SimplexPoint<parameter_size>& other) const {
-		SimplexPoint<parameter_size> result = *this;
-		result *= other;
-		return result;
-	}
-	SimplexPoint<parameter_size> operator/(const  SimplexPoint<parameter_size>& other) const {
-		SimplexPoint<parameter_size> result = *this;
-		result /= other;
-		return result;
-	}
 
-	SimplexPoint<parameter_size> operator*(double scalar) const {
-		SimplexPoint<parameter_size> result = *this;
+	SimplexPoint<parameter_size, AuxilaryParameters> operator*(double scalar) const {
+		SimplexPoint<parameter_size, AuxilaryParameters> result = *this;
 		result *= scalar;
 		return result;
 	}
-	SimplexPoint<parameter_size> operator/(double scalar) const {
-		SimplexPoint<parameter_size> result = *this;
+	SimplexPoint<parameter_size, AuxilaryParameters> operator/(double scalar) const {
+		SimplexPoint<parameter_size, AuxilaryParameters> result = *this;
 		result /= scalar;
 		return result;
 	}
 
-private:
-	bool m_data_valid{ false };
-	//NOTE I am not sure that storing a copy of input arguments in each simplex point is the best approach, might change later
-	//NOTE we could store a reference into the data defined in the simplexFigure, or somewhere else maybe in FitterSettings or in Fitter
-	std::unique_ptr<Data> m_data{ nullptr };
 };
 }
 }
