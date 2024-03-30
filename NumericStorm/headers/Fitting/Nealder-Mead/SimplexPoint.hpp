f6bb3bc8284fa5958900e3c4d516a5cafa439bb6
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2024-03-30 23:58:16 +0100

    added critical comments in SimplexPoint!

--- NumericStorm/headers/Fitting/Nealder-Mead/SimplexPoint.hpp
+++ NumericStorm/headers/Fitting/Nealder-Mead/SimplexPoint.hpp
@@ -14,9 +14,13 @@ namespace Fitting
 template <std::size_t parameter_size, class AuxilaryParameters = AdditionalParameters>
 class SimplexPoint {
 public:
+	//BUG we need a variable for storing the status of referenceData pointer if we are not passing it into the constructor
+	//NOTE we could pass the reference data as this Data, just copy them into the m_data, and evaluate points in constructor
 
+	//BUG to construct the data we need the AuxilaryParameters, we must pass this as the constructor arguments
+	//BUG in this implementation, we are not setting the data_valid, but we calculate the data
 	SimplexPoint(const std::vector<double> arguments, const std::array<double, parameter_size>& parameters)
-		: Parameters<parameter_size, AuxilaryParameters>{ parameters }, m_data{ std::make_unique<Data>(arguments) } {};
+		: Parameters<parameter_size>{ parameters }, m_data{ std::make_unique<Data>(arguments) } {};
 	SimplexPoint() = default;
 	SimplexPoint(const SimplexPoint<parameter_size, AuxilaryParameters>&) = default;
 	SimplexPoint(SimplexPoint<parameter_size, AuxilaryParameters>&&) = default;
@@ -25,15 +29,20 @@ public:
 
 	virtual ~SimplexPoint() = default;
 
+	//NU why we have a two of copy and move constructor?
+	//Q what will happen if the unique ptr will be moved in copy/move constructor? Will we lost the data? 
+	//Q If we leave this copy/move constructor as it is, what will happen with other member variables? I think that they will be lost 
+	//Proposition maybe the data should be a normal object inside the SimplexPoint it will fix our potential problem with loosing it during moving
+	//NOTE At the end of the day, we always could call the move constructor explicitly, we will not have a big problem with performance 
 	SimplexPoint(const Parameters<parameter_size, AuxilaryParameters>& params) : Parameters<parameter_size, AuxilaryParameters>(params), m_data{ std::make_unique<Data>() } {}
 	SimplexPoint(Parameters<parameter_size, AuxilaryParameters>&& params) : Parameters<parameter_size, AuxilaryParameters>(params), m_data{ std::make_unique<Data>() } {}
 	bool isDataValid() const { return m_data_valid; }
 
 private:
-	Parameters<parameter_size, AuxilaryParameters> m_parameters{};
+	//BUG we don't have a method for setting the referenceData
+	//BUG we don't have a method for setting Model* and ErrorModel* 
+	Parameters<parameter_size> m_parameters{};
 	bool m_data_valid{ false };
-	//NOTE I am not sure that storing a copy of input arguments in each simplex point is the best approach, might change later
-	//NOTE we could store a reference into the data defined in the simplexFigure, or somewhere else maybe in FitterSettings or in Fitter
 	std::unique_ptr<Data> m_data{ nullptr };
 	std::shared_ptr<Data> m_referenceData{ nullptr };
 	double m_error{ -1 };
