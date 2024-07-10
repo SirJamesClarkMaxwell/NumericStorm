d83945f5dc2b0be065ec50f8b81777c00879f6a0
Sir James Clarck Maxwell <fzabier@gmail.com>
2024-07-10 23:01:46 +0200

    added adding noise to MC

--- NumericStorm/headers/MonteCarlo/MonteCarlo.hpp
+++ NumericStorm/headers/MonteCarlo/MonteCarlo.hpp
@@ -7,13 +7,14 @@
 #include <execution>
 
 
-namespace MonteCarlo
+namespace NumericStorm::MonteCarlo
 {
-template<class ExecutorWrapper, class Input>
-concept Executor = requires(ExecutorWrapper executorWrapper, Input input, int simulationNumber)
+template<class ExecutorWrapper, class Input, class AnnealingData>
+concept Executor = requires(ExecutorWrapper executorWrapper, Input input, int simulationNumber, AnnealingData & copiedData, const AnnealingData & noisedData)
 {
     { executorWrapper.setUp(input) } -> std::convertible_to<void>;
-    { executorWrapper.execute(simulationNumber) } -> std::convertible_to<void>;
+    { executorWrapper.addNoise(copiedData) } ->std::convertible_to<AnnealingData&>;
+    { executorWrapper.execute(simulationNumber, noisedData) } -> std::convertible_to<void>;
     { executorWrapper.begin() } -> std::convertible_to<typename ExecutorWrapper::Iterator>;
     { executorWrapper.end() } -> std::convertible_to<typename ExecutorWrapper::Iterator>;
 };
@@ -36,8 +37,8 @@ public:
 
 };
 //TODO: add noise!
-template<class ExecutorWrapper, class Input>
-    requires Executor<ExecutorWrapper, Input>
+template<class ExecutorWrapper, class Input, class AnnealingData>
+    requires Executor<ExecutorWrapper, Input, AnnealingData>
 class MonteCarlo
 {
 public:
@@ -45,26 +46,31 @@ public:
     explicit MonteCarlo(ExecutorWrapper executor, Input executorInput, MonteCarloSettings settings)
         :m_executor{ executor }, m_settings{ settings }
     {
-
         m_executor.setUp(executorInput);
         using ExecutionPolicy;
         m_executionPolicy = m_settings.executionPolicy == MultiThread ? execution::par : execution::seq;
     };
-    void runSimulation()
+    void runSimulation(AnnealingData& dataToNoise)
     {
-        std::for_each(m_executionPolicy, m_executor.begin(), m_executor.end()
-            [this](int simulationNumber) {m_executor.execute(simulationNumber);})
+        std::for_each(m_executionPolicy, m_executor.begin(), m_executor.end(),
+            [&](int simulationNumber)
+            {
+                AnnealingData copiedData(dataToNoise);
+                AnnealingData& noisedData = m_executor.addNoise(copiedData);
+                m_executor.execute(simulationNumber, noisedData);
+                //NOTE all data must be sotred in m_executor by construction -> m_executor must to change his state during the execute method -> results of the MCsim must be received from elsewhere
+                    //NOTE  results of the MCsim must be received from elsewhere. Or we will add another struct for this
+                //! we need discus the approach to execution, and where data gonna be stored. 
+                //NOTE we could create the templated struct where all data gonna be stored and MC class will modify the state of that struct, in realization there will be no difference between 
+                    //NOTE m_executor because we need external class which will be modified 
+            });
     };
 
     ~MonteCarlo() = default;
 private:
     ExecutorWrapper m_executor;
     MonteCarloSettings m_settings;
-    namespace execution = std::execution;
-    execution m_executionPolicy;
+    std::execution m_executionPolicy;
 };
-
-
-
 
 }
