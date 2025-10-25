8eb14403d1f4b3d4f6b406ececf52eb11b0b0002
cyberCuber07 <n.eigocuber@gmail.com>
2025-10-25 11:00:47 +0200

    [fix] Make compile on unix systems
    Following fixing issues found in JFM code -
     - this patch makes the code compliant with cpp
    standard as msvc treats it loosely.
    Those issues are:
      - making variables with names same as template arguments
      - remove 'typename' keyword in alias' definees
    
    Signed-off-by: cyberCuber07 <n.eigocuber@gmail.com>

--- NumericStorm/headers/Utils/type_list.hpp
+++ NumericStorm/headers/Utils/type_list.hpp
@@ -16,8 +16,13 @@ namespace NumericStorm::Utils
 	template<class Head, class... Tail>
 	struct pop_front<type_list<Head, Tail...>>
 	{
+#if !defined(JFM_PLATFORM_NIX)
 		using out = typename Head;
 		using type = typename type_list<Tail...>;
+#else
+		using out = Head;
+		using type = type_list<Tail...>;
+#endif
 	};
 
 	template<>
